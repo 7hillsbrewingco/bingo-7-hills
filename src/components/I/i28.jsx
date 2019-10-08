13e4cf7587255222c053b3ba3bf803a5b3ca1b15
Carl Busch <carl@uvdesign.media>
2019-10-08 18:41:20 -0500

    fixed 28

--- src/components/I/i28.jsx
+++ src/components/I/i28.jsx
@@ -19,7 +19,7 @@ class Button extends React.Component {
         className={this.state.button ? "buttonTrue" : "buttonFalse"}
         onClick={this.handleClick}
       >
-        24
+        28
       </button>
     )
   }
